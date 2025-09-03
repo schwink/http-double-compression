e88d81c1ef305fc8be86c3142e9dcaab5932108f
Stephen Schwink <stephen.schwink@gmail.com>
2025-09-03 12:04:37 -0700

    Change title from Compression to Content-Encoding

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ const server = createServer(async (req, res) => {
         res.end(`<!DOCTYPE html>
 <html>
   <head>
-    <title>Compression Test</title>
+    <title>Content-Encoding Test</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
